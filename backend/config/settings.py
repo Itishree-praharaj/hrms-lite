08d4b0318c7b220fa6e10f55bf834a05c14f3b39
Nihal Gupta <144427139+Itishree-praharaj@users.noreply.github.com>
2026-02-19 01:43:48 +0530

    Bug fix

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -144,4 +144,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+# whitenoise (serve static in production)
+MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
